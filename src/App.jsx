8feff1fb6c21935456cc3f659bba492ea4f2558e
zhutongtong <zhutongtong@360.cn>
2021-04-04 14:58:38 +0800

    封装dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const appContext = React.createContext(null)
 }
 
 const A = () => <section>A组件<User/></section>
-const B = () => <section>B组件<UserModifier/></section>
+const B = () => <section>B组件<Wrapper/></section>
 const C = () => <section>C组件</section>
 
 const User = () => {
@@ -39,16 +39,22 @@ const User = () => {
   return <div>用户:{appState.user.name}</div>
 }
 
-const UserModifier = () => {
+const Wrapper = () => {
   const {appState, setAppState} = useContext(appContext)
+  const dispatch = (action) => {
+    setAppState(reducer(appState, action))
+  }
+  return <UserModifier state={appState} dispatch={dispatch} />
+}
 
+
+const UserModifier = ({state, dispatch}) => {
   const onChange = (e) => {
-    const newState = reducer(appState, {type: 'updateUser', payload: {name: e.target.value}})
-    setAppState(newState)
+    dispatch({type: 'updateUser', payload: {name: e.target.value}})
   }
   return (
       <div>
-        <input value={appState.user.name} onChange={onChange}/>
+        <input value={state.user.name} onChange={onChange}/>
       </div>
   )
 }
