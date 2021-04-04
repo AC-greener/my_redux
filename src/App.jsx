ecfea928ea2a0c663b07dedd6f0f83cfc032cdaf
zhutongtong <zhutongtong@360.cn>
2021-04-04 15:22:32 +0800

    add log

--- src/App.jsx
+++ src/App.jsx
@@ -30,11 +30,21 @@ const appContext = React.createContext(null)
   )
 }
 
-const A = () => <section>A组件<UserWrapper/></section>
-const B = () => <section>B组件<UserModifierWrapper x={'xxxxx'}/>内容</section>
-const C = () => <section>C组件</section>
+const A = () => {
+  console.log('A 执行了', Math.random())
+  return <section>A组件<UserWrapper/></section>
+}
+const B = () => {
+  console.log('B 执行了', Math.random())
+  return <section>B组件<UserModifierWrapper x={'xxxxx'}/>内容</section>
+}
+const C = () => {
+console.log('C 执行了', Math.random())
+ return <section>C组件</section>
+}
 
 const User = ({state}) => {
+  console.log('User 执行了',  Math.random())
   return <div>用户:{state.user.name}</div>
 }
 
@@ -54,6 +64,7 @@ const UserModifier = ({state, dispatch, children}) => {
   const onChange = (e) => {
     dispatch({type: 'updateUser', payload: {name: e.target.value}})
   }
+  console.log('UserModifier 执行了', Math.random()  )
   return (
       <div>
         {children}
